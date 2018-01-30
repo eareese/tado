be28cc4c612da1d217bae674de341bd55cc3f0cb
💾 eareese <eareese@eareese.com>
2018-01-30 13:31:29 -0500

    Implement list command

--- cmd/list.go
+++ cmd/list.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 
+	"github.com/eareese/todo/db"
 	"github.com/spf13/cobra"
 )
 
@@ -11,7 +12,19 @@ var listCmd = &cobra.Command{
 	Use:   "list",
 	Short: "List all todos.",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("list called")
+		tasks, err := db.AllTasks()
+		if err != nil {
+			fmt.Println("Something went wrong:", err.Error())
+		}
+		if len(tasks) == 0 {
+			fmt.Println("Nothing to do!")
+			return
+		}
+
+		fmt.Println("You have the following tasks:")
+		for i, task := range tasks {
+			fmt.Printf("%d. %s\n", i+1, task.Value)
+		}
 	},
 }
 
