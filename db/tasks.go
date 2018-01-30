8413815087db37ec7df2b86a65e7637820b638f0
💾 eareese <eareese@eareese.com>
2018-01-30 12:56:28 -0500

    Implement storing tasks in the bucket with conversion helper functions

--- db/tasks.go
+++ db/tasks.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"encoding/binary"
 	"time"
 
 	"github.com/boltdb/bolt"
@@ -25,5 +26,32 @@ func Init(dbPath string) error {
 		_, err := tx.CreateBucketIfNotExists(taskBucket)
 		return err
 	})
+}
 
+func CreateTask(task string) (int, error) {
+	var id int
+	err := db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket(taskBucket)
+		// ignoring this error because it will only error if the transaction is closed
+		id64, _ := b.NextSequence()
+		id = int(id64)
+		key := itobs(int(id64))
+		return b.Put(key, []byte(task))
+	})
+	if err != nil {
+		return -1, err
+	}
+	return id, nil
+}
+
+// conversion helpers needed by BoltDB, since everything stored in it must
+// be a byte slice, so our strings are fine but integers need converting.
+func itobs(v int) []byte {
+	bs := make([]byte, 8)
+	binary.BigEndian.PutUint64(bs, uint64(v))
+	return bs
+}
+
+func bstoi(bs []byte) int {
+	return int(binary.BigEndian.Uint64(bs))
 }
