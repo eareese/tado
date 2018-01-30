3558e2e65d8e909be4b52437c4849a6515316226
💾 eareese <eareese@eareese.com>
2018-01-30 12:45:48 -0500

    Create a database bucket and connect to db in user home

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "github.com/eareese/todo/cmd"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/eareese/todo/cmd"
+	"github.com/eareese/todo/db"
+	homedir "github.com/mitchellh/go-homedir"
+)
 
 func main() {
-	cmd.RootCmd.Execute()
+	home, _ := homedir.Dir()
+	dbPath := filepath.Join(home, "todo.db")
+	must(db.Init(dbPath))
+	must(cmd.RootCmd.Execute())
+}
+
+func must(err error) {
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
 }
